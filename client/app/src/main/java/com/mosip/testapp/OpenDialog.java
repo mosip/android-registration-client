68899f97dd04208c44bbca426c191eab9a3ff268
EricJohnEastwood <ericjohneastwood@gmail.com>
2021-08-27 21:35:51 +0530

    Initial Commit

---
+++ client/app/src/main/java/com/mosip/testapp/OpenDialog.java
@@ -0,0 +1,27 @@
+package com.mosip.testapp;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDialog;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+public class OpenDialog extends AppCompatDialogFragment {
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Clicked").setMessage("Continue?")
+                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+        return builder.create();
+    }
+}
