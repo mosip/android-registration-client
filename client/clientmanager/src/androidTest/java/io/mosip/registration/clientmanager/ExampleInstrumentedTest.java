c64c417459236ae73bee83d0296687c2aa2d462c
EricJohnEastwood <ericjohneastwood@gmail.com>
2021-10-03 19:47:42 +0530

    Add Instrumented Test

--- client/clientmanager/src/androidTest/java/io/mosip/registration/clientmanager/ExampleInstrumentedTest.java
+++ client/clientmanager/src/androidTest/java/io/mosip/registration/clientmanager/ExampleInstrumentedTest.java
@@ -7,7 +7,29 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+
+import org.junit.Before;
+
+
 import static org.junit.Assert.*;
+
+
+import io.mosip.registration.clientmanager.service.crypto.LocalClientCryptoServiceImpl;
+
+
+import android.util.Log;
+import android.R;
+
+import io.mosip.registration.clientmanager.dto.crypto.CryptoRequestDto;
+import io.mosip.registration.clientmanager.dto.crypto.CryptoResponseDto;
+import io.mosip.registration.clientmanager.dto.crypto.PublicKeyRequestDto;
+import io.mosip.registration.clientmanager.dto.crypto.PublicKeyResponseDto;
+import io.mosip.registration.clientmanager.dto.crypto.SignRequestDto;
+import io.mosip.registration.clientmanager.dto.crypto.SignResponseDto;
+import io.mosip.registration.clientmanager.dto.crypto.SignVerifyRequestDto;
+import io.mosip.registration.clientmanager.dto.crypto.SignVerifyResponseDto;
+import io.mosip.registration.clientmanager.spi.crypto.ClientCryptoManagerService;
+import io.mosip.registration.clientmanager.util.ConfigService;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -16,10 +38,125 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
+
+    private static LocalClientCryptoServiceImpl localClientCryptoService;
+
+    //    encryption and decryption keys
+    private static PublicKeyRequestDto publicKeyRequestDto_encdec;
+    private static PublicKeyResponseDto publicKeyResponseDto_encdec;
+
+    //    sign and verify keys
+    private static PublicKeyRequestDto publicKeyRequestDto_sign;
+    private static PublicKeyResponseDto publicKeyResponseDto_sign;
+
+    //    to do initializations
+    @Before
+    public void init(){
+//        creating localclientcrypto object for testing
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("io.mosip.registration.clientmanager.test", appContext.getPackageName());
+        localClientCryptoService=new LocalClientCryptoServiceImpl(appContext);
+        localClientCryptoService.initLocalClientCryptoService(appContext);
+
+//        creating keys for encryption and dcryption testing
+        publicKeyRequestDto_encdec= new PublicKeyRequestDto();
+        publicKeyRequestDto_encdec.setServerProfile("endec");
+        publicKeyResponseDto_encdec = localClientCryptoService.getPublicKey(publicKeyRequestDto_encdec);
+
+//        creating keys for sign and verify
+        publicKeyRequestDto_sign= new PublicKeyRequestDto();
+        publicKeyRequestDto_sign.setServerProfile("sign");
+        publicKeyResponseDto_sign = localClientCryptoService.getPublicKey(publicKeyRequestDto_sign);
+
     }
+
+
+    @Test
+    public void LoadLocalServiceImplTest() {
+        assertNotNull(localClientCryptoService);
+        assertNotNull(publicKeyResponseDto_encdec);
+        assertNotNull(publicKeyResponseDto_sign);
+    }
+
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+
+
+
+    @Test
+    public void getPublicKey_test(){
+//        checking public key for valid types such as endec
+        PublicKeyRequestDto pub_RequestDto = new PublicKeyRequestDto();
+        pub_RequestDto.setServerProfile("endec");
+        PublicKeyResponseDto pub_ResponseDto = localClientCryptoService.getPublicKey(pub_RequestDto);
+        assertNotNull(pub_ResponseDto);
+
+//        checking for sign
+        PublicKeyRequestDto pub_RequestDto_sign = new PublicKeyRequestDto();
+        pub_RequestDto_sign.setServerProfile("sign");
+        PublicKeyResponseDto pub_ResponseDto_sign = localClientCryptoService.getPublicKey(pub_RequestDto_sign);
+        assertNotNull(pub_ResponseDto_sign);
+
+        //        checking for random string
+        PublicKeyRequestDto pub_RequestDto_sign2 = new PublicKeyRequestDto();
+        pub_RequestDto_sign2.setServerProfile("random");
+        PublicKeyResponseDto pub_ResponseDto_sign2 = localClientCryptoService.getPublicKey(pub_RequestDto_sign2);
+        assertNull(pub_ResponseDto_sign2);
+
+    }
+
+    @Test
+    public void encryption_decryption_test(){
+
+//        TESTCASE 1
+
+
+        String input_message="First Message _+& 9901234 ";
+//        encryption
+        CryptoRequestDto cryptoRequestDto = new CryptoRequestDto(
+                input_message, publicKeyResponseDto_encdec.getPublicKey());
+        CryptoResponseDto cryptoResponseDto = localClientCryptoService.encrypt(cryptoRequestDto);
+//        decryption
+        cryptoRequestDto.setValue(cryptoResponseDto.getValue());
+        cryptoResponseDto=localClientCryptoService.decrypt(cryptoRequestDto);
+        assertEquals(input_message,cryptoResponseDto.getValue());
+
+//        TESTCASE 2 (CHECKING AGAINST WRONG INPUT)
+        assertNotEquals("wrong message",cryptoResponseDto.getValue());
+
+
+
+//        TESTCASE 3(against null string)
+        cryptoRequestDto.setValue("");
+        cryptoResponseDto=localClientCryptoService.encrypt(cryptoRequestDto);
+        cryptoRequestDto.setValue(cryptoResponseDto.getValue());
+        cryptoResponseDto=localClientCryptoService.decrypt(cryptoRequestDto);
+        assertEquals("",cryptoResponseDto.getValue());
+
+    }
+
+    @Test
+    public void sign_verify_test(){
+        String signMessage = "messageInput 123";
+        String signed_string;
+
+        SignRequestDto signRequestDto = new SignRequestDto(signMessage);
+        SignResponseDto signResponseDto = localClientCryptoService.sign(signRequestDto);
+        signed_string=signResponseDto.getData();
+//        signing
+
+//        TESTCASE 1 :CHECKING WITH CORRECT MESSAGE
+        SignVerifyRequestDto signVerifyRequestDto=new SignVerifyRequestDto(signMessage, signed_string, publicKeyResponseDto_sign.getPublicKey());
+        SignVerifyResponseDto signVerifyResponseDto=localClientCryptoService.verifySign(signVerifyRequestDto);
+        assertTrue(signVerifyResponseDto.isVerified());
+
+//        TESTCASE 2:WITH WRONG MESSAGE
+        SignVerifyRequestDto signVerifyRequestDto_2=new SignVerifyRequestDto("DUMMY", signed_string, publicKeyResponseDto_sign.getPublicKey());
+        SignVerifyResponseDto signVerifyResponseDto_2=localClientCryptoService.verifySign(signVerifyRequestDto_2);
+        assertFalse(signVerifyResponseDto_2.isVerified());
+
+
+    }
+
 }
