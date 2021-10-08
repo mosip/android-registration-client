be7d2ee74c45406e130646335dac093304c77f2e
Baakal Tesfaye <baakal@gmail.com>
2021-10-08 16:49:53 +0300

    add login ui to registration client

---
+++ registrationclient/app/src/main/java/io/mosip/registration/app/MainActivity.java
@@ -0,0 +1,21 @@
+package io.mosip.registrationclient.mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import io.mosip.registrationclient.mobile.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
---
+++ registrationclient/app/src/main/java/io/mosip/registration/app/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package io.mosip.registrationclient.mobile.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
