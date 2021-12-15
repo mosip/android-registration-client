1c22cb1089fe6670fe13ca12421c628a4ec3b50c
EricJohnEastwood <ericjohneastwood@gmail.com>
2021-12-15 15:18:34 +0530

    Add README.md

--- client/clientmanager/src/androidTest/java/io/mosip/registration/clientmanager/AndroidNetworkingTest.java
+++ client/clientmanager/src/androidTest/java/io/mosip/registration/clientmanager/AndroidNetworkingTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import android.content.Context;
-import android.os.Environment;
 import android.util.Log;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -12,15 +11,12 @@ import androidx.test.platform.app.InstrumentationRegistry;
 
 import com.androidnetworking.AndroidNetworking;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.io.File;
-import java.util.HashMap;
 import java.util.Map;
 
 import io.mosip.registration.clientmanager.dto.http.RequestDto;
