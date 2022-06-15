1f89d06cba69cd3726e0e93329a8516344b0a957
Anshul <Anshulv1401@gmail.com>
2022-06-15 17:42:23 +0530

    Audit service test cases updated

--- client/clientmanager/src/androidTest/java/io/mosip/registration/clientmanager/AuditManagerServiceTest.java
+++ client/clientmanager/src/androidTest/java/io/mosip/registration/clientmanager/AuditManagerServiceTest.java
@@ -19,6 +19,7 @@ import io.mosip.registration.clientmanager.config.SessionManager;
 import io.mosip.registration.clientmanager.constant.AuditEvent;
 import io.mosip.registration.clientmanager.constant.AuditReferenceIdTypes;
 import io.mosip.registration.clientmanager.constant.Components;
+import io.mosip.registration.clientmanager.constant.RegistrationConstants;
 import io.mosip.registration.clientmanager.dao.AuditDao;
 import io.mosip.registration.clientmanager.dao.GlobalParamDao;
 import io.mosip.registration.clientmanager.entity.Audit;
@@ -39,13 +40,13 @@ public class AuditManagerServiceTest {
 
     private static final String PACKET_ID = "10001103911003120220530051317";
     private static final String USER_NAME = "audit@123";
+    private static final String SCREEN_NAME = "Biometric_capture";
 
     private Context appContext;
     private ClientDatabase clientDatabase;
     private AuditRepository auditRepository;
     private GlobalParamRepository globalParamRepository;
     private AuditManagerService auditManagerService;
-    private RegistrationService registrationService;
 
     @Before
     public void init() {
@@ -67,9 +68,9 @@ public class AuditManagerServiceTest {
     }
 
     @Test
-    public void audit_test() {
+    public void auditWillAllParameters_test() {
         auditManagerService = new AuditManagerServiceImpl(appContext, auditRepository, globalParamRepository, null);
-        auditManagerService.audit(AuditEvent.PACKET_AUDIT_JSON_CREATED, Components.REGISTRATION, PACKET_ID, AuditReferenceIdTypes.REGISTRATION_ID.toString());
+        auditManagerService.audit(AuditEvent.LOADED_LOGIN, Components.REGISTRATION, PACKET_ID, AuditReferenceIdTypes.REGISTRATION_ID.toString());
 
         List<Audit> audits = auditRepository.getAuditsFromDate(System.currentTimeMillis() - 5000);
 
@@ -82,9 +83,9 @@ public class AuditManagerServiceTest {
         assertEquals(appContext.getString(R.string.app_name), audits.get(0).getApplicationName());
         assertEquals(PACKET_ID, audits.get(0).getRefId());
         assertEquals(AuditReferenceIdTypes.REGISTRATION_ID.toString(), audits.get(0).getRefIdType());
-        assertEquals(AuditEvent.PACKET_AUDIT_JSON_CREATED.getId(), audits.get(0).getEventId());
-        assertEquals(AuditEvent.PACKET_AUDIT_JSON_CREATED.getName(), audits.get(0).getEventName());
-        assertEquals(AuditEvent.PACKET_AUDIT_JSON_CREATED.getType(), audits.get(0).getEventType());
+        assertEquals(AuditEvent.LOADED_LOGIN.getId(), audits.get(0).getEventId());
+        assertEquals(AuditEvent.LOADED_LOGIN.getName(), audits.get(0).getEventName());
+        assertEquals(AuditEvent.LOADED_LOGIN.getType(), audits.get(0).getEventType());
         assertEquals(Components.REGISTRATION.getId(), audits.get(0).getModuleId());
         assertEquals(Components.REGISTRATION.getName(), audits.get(0).getModuleName());
     }
@@ -92,7 +93,7 @@ public class AuditManagerServiceTest {
     @Test
     public void auditDelete_test() {
         auditManagerService = new AuditManagerServiceImpl(appContext, auditRepository, globalParamRepository, null);
-        auditManagerService.audit(AuditEvent.PACKET_AUDIT_JSON_CREATED, Components.REGISTRATION, PACKET_ID, AuditReferenceIdTypes.REGISTRATION_ID.toString());
+        auditManagerService.audit(AuditEvent.LOADED_LOGIN, Components.REGISTRATION, PACKET_ID, AuditReferenceIdTypes.REGISTRATION_ID.toString());
 
         List<Audit> audits = auditRepository.getAuditsFromDate(System.currentTimeMillis() - 5000);
         assertEquals(1, audits.size());
@@ -102,5 +103,51 @@ public class AuditManagerServiceTest {
 
         audits = auditRepository.getAuditsFromDate(System.currentTimeMillis() - 5000);
         assertEquals(0, audits.size());
+    }
+
+    @Test
+    public void auditWithComponent_test() {
+        auditManagerService = new AuditManagerServiceImpl(appContext, auditRepository, globalParamRepository, null);
+        auditManagerService.audit(AuditEvent.LOADED_LOGIN, Components.REGISTRATION);
+
+        List<Audit> audits = auditRepository.getAuditsFromDate(System.currentTimeMillis() - 5000);
+
+        assertEquals(1, audits.size());
+        assertEquals(USER_NAME, audits.get(0).getSessionUserId());
+        assertEquals(USER_NAME, audits.get(0).getSessionUserName());
+        assertEquals(BuildConfig.BASE_URL, audits.get(0).getHostIp());
+        assertEquals(BuildConfig.BASE_URL, audits.get(0).getHostName());
+        assertEquals(appContext.getString(R.string.app_name), audits.get(0).getApplicationId());
+        assertEquals(appContext.getString(R.string.app_name), audits.get(0).getApplicationName());
+        assertEquals(USER_NAME, audits.get(0).getRefId());
+        assertEquals(AuditReferenceIdTypes.USER_ID.name(), audits.get(0).getRefIdType());
+        assertEquals(AuditEvent.LOADED_LOGIN.getId(), audits.get(0).getEventId());
+        assertEquals(AuditEvent.LOADED_LOGIN.getName(), audits.get(0).getEventName());
+        assertEquals(AuditEvent.LOADED_LOGIN.getType(), audits.get(0).getEventType());
+        assertEquals(Components.REGISTRATION.getId(), audits.get(0).getModuleId());
+        assertEquals(Components.REGISTRATION.getName(), audits.get(0).getModuleName());
+    }
+
+    @Test
+    public void auditWithoutComponent_test() {
+        auditManagerService = new AuditManagerServiceImpl(appContext, auditRepository, globalParamRepository, null);
+        auditManagerService.audit(AuditEvent.NEXT_BUTTON_CLICKED, String.format(RegistrationConstants.REGISTRATION_SCREEN, SCREEN_NAME));
+
+        List<Audit> audits = auditRepository.getAuditsFromDate(System.currentTimeMillis() - 5000);
+
+        assertEquals(1, audits.size());
+        assertEquals(USER_NAME, audits.get(0).getSessionUserId());
+        assertEquals(USER_NAME, audits.get(0).getSessionUserName());
+        assertEquals(BuildConfig.BASE_URL, audits.get(0).getHostIp());
+        assertEquals(BuildConfig.BASE_URL, audits.get(0).getHostName());
+        assertEquals(appContext.getString(R.string.app_name), audits.get(0).getApplicationId());
+        assertEquals(appContext.getString(R.string.app_name), audits.get(0).getApplicationName());
+        assertEquals(USER_NAME, audits.get(0).getRefId());
+        assertEquals(AuditReferenceIdTypes.USER_ID.name(), audits.get(0).getRefIdType());
+        assertEquals(AuditEvent.NEXT_BUTTON_CLICKED.getId(), audits.get(0).getEventId());
+        assertEquals(AuditEvent.NEXT_BUTTON_CLICKED.getName(), audits.get(0).getEventName());
+        assertEquals(AuditEvent.NEXT_BUTTON_CLICKED.getType(), audits.get(0).getEventType());
+        assertEquals(String.format(RegistrationConstants.REGISTRATION_SCREEN, SCREEN_NAME), audits.get(0).getModuleId());
+        assertEquals(String.format(RegistrationConstants.REGISTRATION_SCREEN, SCREEN_NAME), audits.get(0).getModuleName());
     }
 }
