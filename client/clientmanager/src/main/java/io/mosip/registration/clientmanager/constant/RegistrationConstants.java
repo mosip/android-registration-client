ee7778cee2542e13648ea7d75b1efeb79cf7fc5b
Anshul <Anshulv1401@gmail.com>
2022-07-14 20:05:11 +0530

    Minimum bandwidth required to upload setting added into preference.

--- client/clientmanager/src/main/java/io/mosip/registration/clientmanager/constant/RegistrationConstants.java
+++ client/clientmanager/src/main/java/io/mosip/registration/clientmanager/constant/RegistrationConstants.java
@@ -48,8 +48,4 @@ public class RegistrationConstants {
 
     //Audits
     public static final String REGISTRATION_SCREEN = "Registration: %s";
-
-    //Global Params.
-    public static final String PACKET_UPLOAD_MIN_UPSTREAM_BANDWIDTH_KBPS = "PacketUploadMinUpstreamBandwidthKbps";
-
 }
