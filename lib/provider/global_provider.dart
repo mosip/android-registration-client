0a309b4bfe29804aba478091350f72ea1ab2c076
piyush-shukla03_infosys <piyush.shukla03@infosys.com>
2023-06-12 15:43:42 +0530

    Global providers for log in and roles

--- lib/provider/global_provider.dart
+++ lib/provider/global_provider.dart
@@ -9,4 +9,41 @@ class GlobalProvider with ChangeNotifier {
     this._listOfProcesses = value;
     notifyListeners();
   }
+
+  bool _isLoggedIn = false;
+  bool _isOnboarded = false;
+  bool _isDefault = false;
+  bool _isSupervisor = false;
+  bool _isOfficer = false;
+
+  bool get isLoggedIn => _isLoggedIn;
+  bool get isOnboarded => _isOnboarded;
+  bool get isDefault => _isDefault;
+  bool get isSupervisor => _isSupervisor;
+  bool get isOfficer => _isOfficer;
+
+  setIsLoggedIn(bool value) {
+    _isLoggedIn = value;
+    notifyListeners();
+  }
+
+  setIsOnboarded(bool value) {
+    _isOnboarded = value;
+    notifyListeners();
+  }
+
+  setIsDefault(bool value) {
+    _isDefault = value;
+    notifyListeners();
+  }
+
+  setIsSupervisor(bool value) {
+    _isSupervisor = value;
+    notifyListeners();
+  }
+
+  setIsOfficer(bool value) {
+    _isOfficer = value;
+    notifyListeners();
+  }
 }
