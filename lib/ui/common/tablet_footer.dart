af56b424311d338a25dcb36c8e04f7fe04ab1cb3
Piyush7034 <piyushshukla2100@gmail.com>
2023-10-13 12:06:18 +0530

    Removed lint issues
    Signed-off-by: Piyush7034 <piyushshukla2100@gmail.com>

--- lib/ui/common/tablet_footer.dart
+++ lib/ui/common/tablet_footer.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:registration_client/provider/global_provider.dart';
 
