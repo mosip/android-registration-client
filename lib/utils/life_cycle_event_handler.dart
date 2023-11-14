ce7618d139f82493c8e03256980db00f4ca81527
GSPrakashNaiduCyberpwn <146939767+GSPrakashNaiduCyberpwn@users.noreply.github.com>
2023-11-14 09:20:44 +0530

    Fixed the text field hang issue on lock and unlock the device. (#197)
    * Fixed the checkbox issue for gender selection when UI spec set to checkbox
    
    Signed-off-by: G S Prakash <prakash.gs@cyberpwn.com>
    
    * Fixed the textformfield issue while lock and unlocked
    
    Signed-off-by: G S Prakash <prakash.gs@cyberpwn.com>
    
    ---------
    
    Signed-off-by: G S Prakash <prakash.gs@cyberpwn.com>

---
+++ lib/utils/life_cycle_event_handler.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+
+class LifecycleEventHandler extends WidgetsBindingObserver {
+  final AsyncCallback resumeCallBack;
+  final AsyncCallback suspendingCallBack;
+
+  LifecycleEventHandler({
+    required this.resumeCallBack,
+    required this.suspendingCallBack,
+  });
+
+  @override
+  Future<void> didChangeAppLifecycleState(AppLifecycleState state) async {
+    switch (state) {
+      case AppLifecycleState.resumed:
+        if (resumeCallBack != null) {
+          await resumeCallBack();
+        }
+        break;
+      case AppLifecycleState.inactive:
+      case AppLifecycleState.paused:
+      case AppLifecycleState.detached:
+        if (suspendingCallBack != null) {
+          await suspendingCallBack();
+        }
+        break;
+      case AppLifecycleState.hidden:
+    }
+  }
+}
