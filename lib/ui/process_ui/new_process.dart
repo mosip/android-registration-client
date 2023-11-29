3249870769a7690ef37fdc5949702d055435af06
aviral-mishra-hunter <aviralmishra.015@gmail.com>
2023-11-29 15:59:56 +0530

    Issue Resolved RCF-332
    Signed-off-by: aviral-mishra-hunter <aviralmishra.015@gmail.com>

--- lib/ui/process_ui/new_process.dart
+++ lib/ui/process_ui/new_process.dart
@@ -221,6 +221,10 @@ class _NewProcessState extends State<NewProcess> {
           isExceptionPresent = true;
           return isExceptionPresent;
         }
+        if(x.title=="Exception"){
+          isExceptionPresent = true;
+          return isExceptionPresent;
+        }
       });
       return isExceptionPresent;
     }
