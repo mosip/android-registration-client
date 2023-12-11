49ba7215cd7b05a112d39318bb8e3bc35f7114b9
Aditya Singh(aditya.singh509) <aditya.singh509@infosys.com>
2023-12-11 12:43:08 +0530

    ui added
    Signed-off-by: Aditya Singh(aditya.singh509) <aditya.singh509@infosys.com>

--- lib/ui/process_ui/widgets/age_date_control.dart
+++ lib/ui/process_ui/widgets/age_date_control.dart
@@ -6,10 +6,12 @@ import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:registration_client/pigeon/biometrics_pigeon.dart';
 import 'package:registration_client/provider/registration_task_provider.dart';
+import 'package:registration_client/utils/app_config.dart';
 import 'package:registration_client/utils/app_style.dart';
 
 import '../../../model/field.dart';
 import '../../../provider/global_provider.dart';
+import 'custom_cupertino_picker.dart';
 import 'custom_label.dart';
 
 class AgeDateControl extends StatefulWidget {
@@ -24,56 +26,13 @@ class AgeDateControl extends StatefulWidget {
 }
 
 class _AgeDateControlState extends State<AgeDateControl> {
-  TextEditingController dayController = TextEditingController();
-
-  TextEditingController monthController = TextEditingController();
-  TextEditingController yearController = TextEditingController();
+  TextEditingController dateController = TextEditingController();
   TextEditingController ageController = TextEditingController();
-
-  final dayFocus = FocusNode();
-  final monthFocus = FocusNode();
-  final yearFocus = FocusNode();
 
   @override
   void initState() {
     _getSavedDate();
     super.initState();
-  }
-
-  @override
-  void dispose() {
-    dayController.dispose();
-    monthController.dispose();
-    yearController.dispose();
-
-    dayFocus.dispose();
-    monthFocus.dispose();
-    yearFocus.dispose();
-
-    super.dispose();
-  }
-
-  void focusNextField(FocusNode currentFocus, FocusNode nextFocus) {
-    currentFocus.unfocus();
-    FocusScope.of(context).requestFocus(nextFocus);
-  }
-
-  void _removeFocusFromAll(String currentTab) {
-    switch (currentTab) {
-      case "day":
-        monthFocus.unfocus();
-        yearFocus.unfocus();
-        break;
-      case "month":
-        dayFocus.unfocus();
-        yearFocus.unfocus();
-        break;
-      case "year":
-        dayFocus.unfocus();
-        monthFocus.unfocus();
-        break;
-      default:
-    }
   }
 
   int calculateYearDifference(DateTime date1, DateTime date2) {
@@ -86,8 +45,8 @@ class _AgeDateControlState extends State<AgeDateControl> {
   }
 
   _calculateAgeFromDOB() {
-    DateTime date = DateTime.parse(
-        "${yearController.text}-${monthController.text.padLeft(2, '0')}-${dayController.text.padLeft(2, '0')}");
+    String dateString = dateController.text;
+    DateTime date = DateTime.parse(dateString.replaceAll("/", "-"));
     DateTime currentDate = DateTime.now();
     if (date.compareTo(currentDate) < 0) {
       ageController.text =
@@ -97,50 +56,24 @@ class _AgeDateControlState extends State<AgeDateControl> {
     }
   }
 
-  String? fieldValidation(value, message) {
-    try {
-      String targetDateString = widget.field.format ??
-          "yyyy/MM/dd"
-              .replaceAll('dd', dayController.text.padLeft(2, '0'))
-              .replaceAll('MM', monthController.text.padLeft(2, '0'))
-              .replaceAll('yyyy', yearController.text);
-
-      if (value == "") {
-        return 'Empty';
-      }
-      if (!widget.validation.hasMatch(targetDateString)) {
-        return message;
-      }
-      return null;
-    } catch (e) {
-      log("error");
-      return "";
-    }
-  }
-
-  _invalidDateText() {
-    DateTime date = DateTime.parse(
-        "${yearController.text}-${monthController.text.padLeft(2, '0')}-${dayController.text.padLeft(2, '0')}");
-    DateTime currentDate = DateTime.now();
-    if (date.compareTo(currentDate) > 0) {
-      return "Invalid date!";
-    }
-    return null;
-  }
-
   void saveData() {
+    if (dateController.text == "") {
+      return;
+    }
+    List<String> date = dateController.text.split("/");
+    print(date.toString());
     String targetDateString = widget.field.format ??
         "yyyy/MM/dd"
-            .replaceAll('dd', dayController.text.padLeft(2, '0'))
-            .replaceAll('MM', monthController.text.padLeft(2, '0'))
-            .replaceAll('yyyy', yearController.text);
+            .replaceAll('dd', date[0])
+            .replaceAll('MM', date[1])
+            .replaceAll('yyyy', date[2]);
 
     context.read<RegistrationTaskProvider>().setDateField(
           widget.field.id ?? "",
           widget.field.subType ?? "",
-          dayController.text.padLeft(2, '0'),
-          monthController.text.padLeft(2, '0'),
-          yearController.text,
+          date[0],
+          date[1],
+          date[2],
         );
     context.read<GlobalProvider>().setInputMapValue(
           widget.field.id!,
@@ -162,15 +95,12 @@ class _AgeDateControlState extends State<AgeDateControl> {
       String savedDate =
           context.read<GlobalProvider>().fieldInputValue[widget.field.id];
       DateTime parsedDate = DateFormat(targetDateFormat).parse(savedDate);
-      dayController.text = parsedDate.day.toString().padLeft(2, '0');
-      monthController.text = parsedDate.month.toString().padLeft(2, '0');
-      yearController.text = parsedDate.year.toString();
-      ageController.text = calculateYearDifference(
-              DateTime.parse(
-                  "${yearController.text}-${monthController.text.padLeft(2, '0')}-${dayController.text.padLeft(2, '0')}"),
-              DateTime.now())
-          .abs()
-          .toString();
+      setState(() {
+        dateController.text = savedDate;
+        ageController.text = calculateYearDifference(parsedDate, DateTime.now())
+            .abs()
+            .toString();
+      });
     }
   }
 
@@ -178,9 +108,70 @@ class _AgeDateControlState extends State<AgeDateControl> {
     int age = int.parse(value);
     DateTime currentDate = DateTime.now();
     DateTime calculatedDate = DateTime(currentDate.year - age, 1, 1);
-    dayController.text = calculatedDate.day.toString().padLeft(2, '0');
-    monthController.text = calculatedDate.month.toString().padLeft(2, '0');
-    yearController.text = calculatedDate.year.toString();
+    setState(() {
+      dateController.text = DateFormat(widget.field.format ?? "yyyy/MM/dd")
+          .format(calculatedDate);
+    });
+  }
+
+  void showBottomPopup(BuildContext context) {
+    print("button pressed");
+    showBottomSheet(
+        backgroundColor: Colors.grey.shade100,
+        context: context,
+        enableDrag: true,
+        elevation: 5,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        builder: (context) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 0),
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height / 2,
+              child: CustomCupertinoDatePicker(
+                maxDate: DateTime.now(),
+                minDate: DateTime(1920),
+                itemExtent: 75,
+                diameterRatio: 10,
+                selectionOverlay: Container(
+                  width: double.infinity,
+                  height: 50,
+                  decoration: const BoxDecoration(
+                    border: Border.symmetric(
+                      horizontal: BorderSide(color: Colors.grey, width: 0.25),
+                    ),
+                  ),
+                ),
+                selectedStyle: const TextStyle(
+                  color: Colors.black54,
+                  fontWeight: FontWeight.w600,
+                  fontSize: 24,
+                ),
+                unselectedStyle: TextStyle(
+                  color: Colors.grey[800],
+                  fontSize: 18,
+                ),
+                disabledStyle: TextStyle(
+                  color: Colors.grey[400],
+                  fontSize: 18,
+                ),
+                onSelectedItemChanged: (selectedDate) {
+                  String targetDateString = widget.field.format ??
+                      "yyyy/MM/dd"
+                          .replaceAll(
+                              'dd', selectedDate.day.toString().padLeft(2, "0"))
+                          .replaceAll('MM',
+                              selectedDate.month.toString().padLeft(2, "0"))
+                          .replaceAll('yyyy', selectedDate.year.toString());
+                  setState(() {
+                    dateController.text = targetDateString;
+                  });
+                  _calculateAgeFromDOB();
+                  saveData();
+                },
+              ),
+            ),
+          );
+        });
   }
 
   @override
@@ -204,125 +195,26 @@ class _AgeDateControlState extends State<AgeDateControl> {
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
                     Flexible(
+                      flex: 3,
                       child: TextFormField(
-                        onTap: () => _removeFocusFromAll("day"),
-                        autovalidateMode: AutovalidateMode.onUserInteraction,
-                        validator: (value) {
-                          String? valid = fieldValidation(value, "dd");
-                          if (valid == null) {
-                            return _invalidDateText();
-                          }
-                          return valid;
-                        },
-                        onChanged: (value) {
-                          if (value.length == 2 &&
-                              monthController.text.length == 2 &&
-                              yearController.text.length == 4) {
-                            _calculateAgeFromDOB();
-                          } else {
-                            ageController.text = "";
-                          }
-                          if (value.length >= 2) {
-                            focusNextField(dayFocus, monthFocus);
-                          }
-                          saveData();
-                        },
-                        maxLength: 2,
-                        focusNode: dayFocus,
-                        keyboardType: TextInputType.number,
-                        controller: dayController,
+                        readOnly: true,
+                        controller: dateController,
+                        onTap: (() {
+                          showBottomPopup(context);
+                        }),
+                        textAlign: TextAlign.center,
                         decoration: InputDecoration(
                           contentPadding: const EdgeInsets.symmetric(
                               vertical: 12, horizontal: 16),
-                          hintStyle:const TextStyle(
-                              color: AppStyle.appBlackShade3, fontSize: 14),
-                          counterText: "",
-                          hintText: 'DD',
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(8.0),
-                            borderSide: const BorderSide(
-                                color: Color(0xff9B9B9F), width: 1),
+                          hintStyle: const TextStyle(
+                            color: AppStyle.appBlackShade3,
+                            fontSize: 14,
                           ),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(width: 8.0),
-                    Flexible(
-                      child: TextFormField(
-                        onTap: () => _removeFocusFromAll("month"),
-                        autovalidateMode: AutovalidateMode.onUserInteraction,
-                        validator: (value) {
-                          String? valid = fieldValidation(value, "MM");
-                          if (valid == null) {
-                            return _invalidDateText();
-                          }
-                          return valid;
-                        },
-                        onChanged: (value) {
-                          if (value.length == 2 &&
-                              monthController.text.length == 2 &&
-                              yearController.text.length == 4) {
-                            _calculateAgeFromDOB();
-                          } else {
-                            ageController.text = "";
-                          }
-                          if (value.length >= 2) {
-                            focusNextField(monthFocus, yearFocus);
-                          }
-                          saveData();
-                        },
-                        maxLength: 2,
-                        focusNode: monthFocus,
-                        keyboardType: TextInputType.number,
-                        controller: monthController,
-                        decoration: InputDecoration(
-                          contentPadding: const EdgeInsets.symmetric(
-                              vertical: 12, horizontal: 16),
-                          hintStyle:const TextStyle(
-                              color: AppStyle.appBlackShade3, fontSize: 14),
-                          counterText: "",
-                          hintText: 'MM',
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(8.0),
-                            borderSide: const BorderSide(
-                                color: Color(0xff9B9B9F), width: 1),
+                          hintText: widget.field.format ?? "yyyy/MM/dd",
+                          prefixIcon: Icon(
+                            Icons.calendar_month,
+                            color: solidPrimary,
                           ),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(width: 8.0),
-                    Flexible(
-                      child: TextFormField(
-                        validator: (value) {
-                          String? valid = fieldValidation(value, "yyyy");
-                          if (valid == null) {
-                            return _invalidDateText();
-                          }
-                          return valid;
-                        },
-                        onChanged: (value) {
-                          if (value.length == 4 &&
-                              dayController.text.length == 2 &&
-                              monthController.text.length == 2) {
-                            _calculateAgeFromDOB();
-                          } else {
-                            ageController.text = "";
-                          }
-                          saveData();
-                        },
-                        onTap: () => _removeFocusFromAll("year"),
-                        autovalidateMode: AutovalidateMode.onUserInteraction,
-                        maxLength: 4,
-                        focusNode: yearFocus,
-                        controller: yearController,
-                        keyboardType: TextInputType.number,
-                        decoration: InputDecoration(
-                          contentPadding: const EdgeInsets.symmetric(
-                              vertical: 12, horizontal: 16),
-                          hintStyle:const TextStyle(
-                              color: AppStyle.appBlackShade3, fontSize: 14),
-                          counterText: "",
-                          hintText: 'YYYY',
                           border: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(8.0),
                             borderSide: const BorderSide(
@@ -335,8 +227,8 @@ class _AgeDateControlState extends State<AgeDateControl> {
                     const Text("OR"),
                     const SizedBox(width: 12),
                     Flexible(
+                      flex: 1,
                       child: TextFormField(
-                        // readOnly: true,
                         controller: ageController,
                         keyboardType: TextInputType.number,
                         onChanged: (value) {
@@ -344,15 +236,13 @@ class _AgeDateControlState extends State<AgeDateControl> {
                             _getDateFromAge(value);
                             saveData();
                           } else {
-                            dayController.text = "";
-                            monthController.text = "";
-                            yearController.text = "";
+                            dateController.text = "";
                           }
                         },
                         decoration: InputDecoration(
                           contentPadding: const EdgeInsets.symmetric(
                               vertical: 12, horizontal: 16),
-                          hintStyle:const TextStyle(
+                          hintStyle: const TextStyle(
                               color: AppStyle.appBlackShade3, fontSize: 14),
                           hintText: 'Age',
                           border: OutlineInputBorder(
---
+++ lib/ui/process_ui/widgets/gender_control.dart
@@ -0,0 +1,241 @@
+import 'package:flutter/material.dart';
+
+import 'package:provider/provider.dart';
+import 'package:registration_client/provider/registration_task_provider.dart';
+import 'package:registration_client/utils/app_config.dart';
+import 'package:registration_client/utils/app_style.dart';
+
+import '../../../model/field.dart';
+import '../../../provider/global_provider.dart';
+import 'custom_label.dart';
+
+class GenderControl extends StatefulWidget {
+  const GenderControl(
+      {super.key, required this.field, required this.validation});
+
+  final Field field;
+  final RegExp validation;
+
+  @override
+  State<GenderControl> createState() => _CustomDynamicDropDownState();
+}
+
+class _CustomDynamicDropDownState extends State<GenderControl> {
+  String? selected;
+
+  @override
+  void initState() {
+    if (context
+        .read<GlobalProvider>()
+        .fieldInputValue
+        .containsKey(widget.field.id ?? "")) {
+      _getSelectedValueFromMap("eng");
+    }
+    super.initState();
+  }
+
+  void saveData(value) {
+    if (value != null) {
+      if (widget.field.type == 'simpleType') {
+        context
+            .read<RegistrationTaskProvider>()
+            .addSimpleTypeDemographicField(widget.field.id ?? "", value, "eng");
+      } else {
+        context
+            .read<RegistrationTaskProvider>()
+            .addDemographicField(widget.field.id ?? "", value);
+      }
+    }
+  }
+
+  void _saveDataToMap(value) {
+    if (value != null) {
+      if (widget.field.type == 'simpleType') {
+        context.read<GlobalProvider>().setLanguageSpecificValue(
+              widget.field.id ?? "",
+              value!,
+              "eng",
+              context.read<GlobalProvider>().fieldInputValue,
+            );
+      } else {
+        context.read<GlobalProvider>().setInputMapValue(
+              widget.field.id ?? "",
+              value!,
+              context.read<GlobalProvider>().fieldInputValue,
+            );
+      }
+    }
+  }
+
+  void _getSelectedValueFromMap(String lang) {
+    String response = "";
+    if (widget.field.type == 'simpleType') {
+      if ((context.read<GlobalProvider>().fieldInputValue[widget.field.id ?? ""]
+              as Map<String, dynamic>)
+          .containsKey(lang)) {
+        response = context
+            .read<GlobalProvider>()
+            .fieldInputValue[widget.field.id ?? ""][lang];
+      }
+    } else {
+      response =
+          context.read<GlobalProvider>().fieldInputValue[widget.field.id ?? ""];
+    }
+    setState(() {
+      selected = response;
+    });
+  }
+
+  Future<List<String?>> _getFieldValues(String fieldId, String langCode) async {
+    return await context
+        .read<RegistrationTaskProvider>()
+        .getFieldValues(fieldId, langCode);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: _getFieldValues(widget.field.subType!, "eng"),
+        builder: (BuildContext context, AsyncSnapshot<List<String?>> snapshot) {
+          return Card(
+            elevation: 0,
+            margin: const EdgeInsets.symmetric(vertical: 1, horizontal: 12),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CustomLabel(field: widget.field),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  snapshot.hasData
+                      ? Row(
+                          children: snapshot.data!.map((e) {
+                          bool selected = false;
+                          return Padding(
+                            padding: const EdgeInsets.only(right: 6),
+                            child: Column(
+                                children: context
+                                    .read<GlobalProvider>()
+                                    .chosenLang
+                                    .map((lang) {
+                              if (lang == "English") {
+                                return ChoiceChip(
+                                  label: SizedBox(
+                                    width: 50,
+                                    child: Text(
+                                      e ?? "",
+                                      textAlign: TextAlign.center,
+                                    ),
+                                  ),
+                                  selected: selected,
+                                  elevation: 0,
+                                  disabledColor: Colors.white,
+                                  selectedColor: solidPrimary,
+                                  shape: selected
+                                      ? null
+                                      : RoundedRectangleBorder(
+                                          side: BorderSide(
+                                              color: solidPrimary, width: 1),
+                                          borderRadius:
+                                              BorderRadius.circular(32)),
+                                  labelPadding: const EdgeInsets.symmetric(
+                                      vertical: 4, horizontal: 8),
+                                  labelStyle: TextStyle(
+                                      fontWeight: FontWeight.w400,
+                                      fontSize: 12,
+                                      color: selected
+                                          ? Colors.white
+                                          : Colors.black54),
+                                );
+                              } else {
+                                return ChoiceChip(
+                                  label: SizedBox(
+                                    width: 50,
+                                    child: Text(
+                                      e ?? "",
+                                      textAlign: TextAlign.center,
+                                    ),
+                                  ),
+                                  selected: selected,
+                                  selectedColor: const Color(0xffEFF3FF),
+                                  elevation: 0,
+                                  disabledColor: Colors.white,
+                                  shape: selected
+                                      ? null
+                                      : RoundedRectangleBorder(
+                                          side: const BorderSide(
+                                              color: Color(0xffC2D0F2),
+                                              width: 1),
+                                          borderRadius:
+                                              BorderRadius.circular(32)),
+                                  labelPadding: const EdgeInsets.symmetric(
+                                      vertical: 4, horizontal: 8),
+                                  labelStyle: TextStyle(
+                                      fontWeight: FontWeight.w400,
+                                      fontSize: 12,
+                                      color: selected
+                                          ? solidPrimary
+                                          : Colors.black54),
+                                );
+                              }
+                            }).toList()),
+                          );
+                        }).toList())
+
+                      // DropdownButtonFormField(
+                      //     icon: const Icon(null),
+                      //     decoration: InputDecoration(
+                      //       contentPadding:
+                      //           const EdgeInsets.symmetric(horizontal: 16.0),
+                      //       border: OutlineInputBorder(
+                      //         borderRadius: BorderRadius.circular(8.0),
+                      //         borderSide: const BorderSide(
+                      //           color: Colors.grey,
+                      //           width: 1.0,
+                      //         ),
+                      //       ),
+                      //       hintText: "Select Option",
+                      //       hintStyle: const TextStyle(
+                      //         color: AppStyle.appBlackShade3,
+                      //       ),
+                      //     ),
+                      //     items: snapshot.data!
+                      //         .map((option) => DropdownMenuItem(
+                      //               value: option,
+                      //               child: Text(option!),
+                      //             ))
+                      //         .toList(),
+                      //     autovalidateMode: AutovalidateMode.onUserInteraction,
+                      //     value: selected,
+                      //     validator: (value) {
+                      //       if (!widget.field.required! &&
+                      //           widget.field.requiredOn!.isEmpty) {
+                      //         return null;
+                      //       }
+                      //       if (value == null || value.isEmpty) {
+                      //         return 'Please enter a value';
+                      //       }
+                      //       if (!widget.validation.hasMatch(value)) {
+                      //         return 'Invalid input';
+                      //       }
+                      //       return null;
+                      //     },
+                      //     onChanged: (value) {
+                      //       saveData(value);
+                      //       _saveDataToMap(value);
+                      //       setState(() {
+                      //         selected = value!;
+                      //       });
+                      //     },
+                      //   )
+
+                      : const SizedBox.shrink(),
+                ],
+              ),
+            ),
+          );
+        });
+  }
+}
