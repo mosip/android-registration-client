5493489c31f650487155b7266ed09529ff735e61
Piyush7034 <47858366+Piyush7034@users.noreply.github.com>
2024-01-24 19:04:31 +0530

    Changed MPL license to MIT license (#256)

--- lib/ui/process_ui/widgets/biometric_capture_control_landscape.dart
+++ lib/ui/process_ui/widgets/biometric_capture_control_landscape.dart
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) Modular Open Source Identity Platform
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+*/
+
 // This view is now deprecated and is not being used.
 import 'package:flutter/material.dart';
 
