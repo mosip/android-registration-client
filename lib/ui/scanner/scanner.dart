82c1ecdfc53eb6a91ac43eecb4c2a6d024a6b88f
Piyush7034 <piyushshukla2100@gmail.com>
2024-01-24 17:20:24 +0530

    Changed MPL license to MIT license
    Signed-off-by: Piyush7034 <piyushshukla2100@gmail.com>

--- lib/ui/scanner/scanner.dart
+++ lib/ui/scanner/scanner.dart
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) Modular Open Source Identity Platform
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+*/
+
 import 'dart:io';
 
 import 'package:dotted_border/dotted_border.dart';
