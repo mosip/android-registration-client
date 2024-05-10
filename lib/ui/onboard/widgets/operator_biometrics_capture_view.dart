1a0add6d66f39d3c077553b69d2d3d20b59f6a67
Sachin s p <52343650+SachinPremkumar@users.noreply.github.com>
2024-05-10 13:50:29 +0530

    fixed biometric exception issue (#352)

--- lib/ui/onboard/widgets/operator_biometrics_capture_view.dart
+++ lib/ui/onboard/widgets/operator_biometrics_capture_view.dart
@@ -99,7 +99,14 @@ class _OperatorBiometricsCaptureState
                       "assets/images/Group 57548@2x.png",
                     )
                         : Image.asset(
-                      "assets/images/Group 57745@2x.png",
+                            "assets/images/Group 57745@2x.png",
+                          )),
+              if (!biometricAttributeData.exceptions.contains(false))
+                Positioned(
+                    top: 15,
+                    right: 15,
+                    child: Image.asset(
+                      "assets/images/Group 57548@2x.png",
                     )),
               if (biometricAttributeData.isScanned == true)
                 Positioned(
