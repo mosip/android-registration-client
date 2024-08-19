75f6d1e057cf04143efdf02e71a2dd869bf8574c
G S Prakash Naidu <146939767+GSPrakashNaidu2662@users.noreply.github.com>
2024-08-19 12:03:59 +0530

    [RCF-741] (#431)
    * added collab env to gradle
    
    Signed-off-by: G S Prakash <prakash.gs@cyberpwn.com>
    
    * changed server url
    
    Signed-off-by: G S Prakash <prakash.gs@cyberpwn.com>
    
    * Added the validation for applicant biometric threshold
    
    Signed-off-by: G S Prakash <prakash.gs@cyberpwn.com>
    
    ---------
    
    Signed-off-by: G S Prakash <prakash.gs@cyberpwn.com>

--- lib/ui/process_ui/new_process.dart
+++ lib/ui/process_ui/new_process.dart
@@ -408,6 +408,10 @@ class _NewProcessState extends State<NewProcess> with WidgetsBindingObserver {
 
                     break;
                   }
+                  if(globalProvider.isValidBiometricCapture){
+                    isValid = false;
+                    break;
+                  }
                 }
               }
             }
@@ -462,6 +466,10 @@ class _NewProcessState extends State<NewProcess> with WidgetsBindingObserver {
                           count) {
                         isValid = false;
 
+                        break;
+                      }
+                      if(globalProvider.isValidBiometricCapture){
+                        isValid = false;
                         break;
                       }
                     }
