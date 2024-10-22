21bee1b1bac41b0eda49d984ddd25d3549b2b472
Sachin S P <sachin.sp@cyberpwn.com>
2024-10-22 16:17:40 +0530

    bug fixed on qa-plan
    Signed-off-by: Sachin S P <sachin.sp@cyberpwn.com>

--- android/clientmanager/src/main/java/io/mosip/registration/clientmanager/config/AppModule.java
+++ android/clientmanager/src/main/java/io/mosip/registration/clientmanager/config/AppModule.java
@@ -287,8 +287,8 @@ public class AppModule {
 
     @Provides
     @Singleton
-    PreRegZipHandlingService PreRegZipHandlingService(ApplicantValidDocumentDao applicantValidDocumentDao, IdentitySchemaRepository identitySchemaService, ClientCryptoManagerService clientCryptoFacade, RegistrationService registrationService, CryptoManagerService cryptoManagerService, PacketKeeper packetKeeper, IPacketCryptoService iPacketCryptoService, MasterDataService masterDataService) {
-        return new PreRegZipHandlingServiceImpl(appContext, applicantValidDocumentDao, identitySchemaService, clientCryptoFacade, registrationService, cryptoManagerService, packetKeeper, iPacketCryptoService, masterDataService);
+    PreRegZipHandlingService PreRegZipHandlingService(ApplicantValidDocumentDao applicantValidDocumentDao, IdentitySchemaRepository identitySchemaService, ClientCryptoManagerService clientCryptoFacade, RegistrationService registrationService, CryptoManagerService cryptoManagerService, PacketKeeper packetKeeper, IPacketCryptoService iPacketCryptoService, MasterDataService masterDataService,GlobalParamRepository globalParamRepository) {
+        return new PreRegZipHandlingServiceImpl(appContext, applicantValidDocumentDao, identitySchemaService, clientCryptoFacade, registrationService, cryptoManagerService, packetKeeper, iPacketCryptoService, masterDataService,globalParamRepository);
     }
 
     @Provides
