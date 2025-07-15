71175a5a69b9bc87f825169932c8345ec6523c73
Madhuravas reddy <madhu@mosip.io>
2025-07-15 14:28:55 +0530

    RCF-1167 Removed download json btn for machine details
    Signed-off-by: Madhuravas reddy <madhu@mosip.io>

--- lib/ui/machine_keys.dart
+++ lib/ui/machine_keys.dart
@@ -108,22 +108,6 @@ class _MachineKeysState extends State<MachineKeys> {
             SizedBox(
               height: 20.h,
             ),
-            _downloadButton(
-              title: appLocalizations.download_json,
-              onTap: () {
-                FileStorage.writeCounter(machineDetails, "machine_details.txt")
-                    .then((value) {
-                  showInSnackBar(appLocalizations.download_message);
-                });
-              },
-            ),
-            // SizedBox(
-            //   height: 20.h,
-            // ),
-            // _downloadButton(
-            //   title: appLocalizations.share,
-            //   onTap: () {},
-            // ),
             SizedBox(
               height: 30.h,
             ),
