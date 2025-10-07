26bc96507148503b3e9aa880a01a05d98982624a
Sachin S  P <52343650+SachinPremkumar@users.noreply.github.com>
2025-10-07 10:48:34 +0530

    RCF-92 : Global Config Settings Implementation (#582)
    * RCF-92 global configue settings
    
    Signed-off-by: sachin.sp <sachin.sp@cyberpwn.com>
    
    * RCF-92 global configue settings
    
    Signed-off-by: sachin.sp <sachin.sp@cyberpwn.com>
    
    * RCF-92 : global configue settings implementation
    
    Signed-off-by: sachin.sp <sachin.sp@cyberpwn.com>
    
    * RCF-92 : global configue settings implementation
    
    Signed-off-by: sachin.sp <sachin.sp@cyberpwn.com>
    
    * RCF- 92: removed log's settings
    
    Signed-off-by: sachin.sp <sachin.sp@cyberpwn.com>
    
    * RCF- 92: updated the review comments
    
    Signed-off-by: sachin.sp <sachin.sp@cyberpwn.com>
    
    * RCF- 92: updated the review comments
    
    Signed-off-by: sachin.sp <sachin.sp@cyberpwn.com>
    
    ---------
    
    Signed-off-by: sachin.sp <sachin.sp@cyberpwn.com>
    Co-authored-by: sachin.sp <sachin.sp@cyberpwn.com>

---
+++ android/clientmanager/src/main/java/io/mosip/registration/clientmanager/dao/LocalConfigDAO.java
@@ -0,0 +1,30 @@
+package io.mosip.registration.clientmanager.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import io.mosip.registration.clientmanager.entity.PermittedLocalConfig;
+
+/**
+ * DAO interface for local configuration operations
+ * Handles both permitted configurations and local preferences
+ */
+public interface LocalConfigDAO {
+
+    /**
+     * Get permitted configurations by type
+     */
+    List<String> getPermittedConfigurations(String configType);
+
+    /**
+     * Get local configurations as a map (name -> value)
+     */
+    Map<String, String> getLocalConfigurations();
+
+    /**
+     * Modify configurations by saving local preferences
+     */
+    void modifyConfigurations(Map<String, String> localPreferences);
+
+    void cleanUpLocalPreferences();
+}
