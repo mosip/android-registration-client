10a7693741af433514ab9f6238af9ee77e4754bc
Rakshithasai123 <rakshithasai2002@gmail.com>
2025-11-07 15:26:56 +0530

    Fixed-In the global config settings page the Global Config settings heading is not displaying (#607)
    Signed-off-by: Rakshithasai123 <rakshithasai2002@gmail.com>
    Signed-off-by: sachin.sp <sachin.sp@cyberpwn.com>

--- lib/ui/settings/settings_screen.dart
+++ lib/ui/settings/settings_screen.dart
@@ -179,7 +179,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
       case 'ScheduledJobsSettingsController':
         return ScheduledJobsSettings(jobJsonList: activeJobs);
       case 'GlobalConfigSettingsController':
-        return const GlobalConfigSettingsTab();
+        return GlobalConfigSettingsTab(settings: settings,selectedLan: selectedLang);
       case 'DeviceSettingsController':
         return DeviceSettingsTab(settings: settings, selectedLan: selectedLang);
       default:
