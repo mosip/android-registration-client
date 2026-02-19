62f28901a7d9a6aeacf79ba56201c1344601d031
damodarguru <damodar.g@cyberpwn.com>
2026-02-19 19:11:03 +0530

    MOSIP-44175:ARC - Run the ARC UI automation in French language
    Signed-off-by: damodarguru <damodar.g@cyberpwn.com>

--- ui-test/src/main/java/regclient/BaseTest/BaseTest.java
+++ ui-test/src/main/java/regclient/BaseTest/BaseTest.java
@@ -2,7 +2,6 @@ package regclient.BaseTest;
 
 import io.appium.java_client.AppiumDriver;
 import regclient.driver.DriverManager;
-import regclient.page.BasePage;
 
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
