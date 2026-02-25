d572dc5d1c25a8a23463ded22e37cd69c56c5cc3
damodarguru <damodar.g@cyberpwn.com>
2026-02-25 16:10:27 +0530

    MOSIP-44485:ARC - Export packet to local device
    Signed-off-by: damodarguru <damodar.g@cyberpwn.com>

--- ui-test/src/main/java/regclient/pages/english/PreviewPageEnglish.java
+++ ui-test/src/main/java/regclient/pages/english/PreviewPageEnglish.java
@@ -49,7 +49,7 @@ public class PreviewPageEnglish extends PreviewPage {
 
 	@AndroidFindBy(accessibility = "Lost UIN")
 	private WebElement lostUinTitle;
-	
+
 	@AndroidFindBy(accessibility = "Biometric correction")
 	private WebElement biometricCorrectionTitle;
 
@@ -104,12 +104,17 @@ public class PreviewPageEnglish extends PreviewPage {
 				"new UiScrollable(new UiSelector().scrollable(true).instance(0)).scrollIntoView(new UiSelector().descriptionContains(\""
 						+ FetchUiSpec.getTitleUsingId("UPDATE") + "\"))")));
 	}
-	
-	@SuppressWarnings("deprecation")
+
 	public boolean isBiometricCorrectionTitleDisplayed() {
-		return isElementDisplayed(findElementWithRetry(MobileBy.AndroidUIAutomator(
-				"new UiScrollable(new UiSelector().scrollable(true).instance(0)).scrollIntoView(new UiSelector().descriptionContains(\""
-						+ FetchUiSpec.getTitleUsingId("BIO") + "\"))")));
+	    try {
+	        driver.findElement(MobileBy.AndroidUIAutomator(
+	            "new UiScrollable(new UiSelector().scrollable(true))" +
+	            ".scrollIntoView(new UiSelector().description(\"Biometric correction\"))"
+	        ));
+	        return true;
+	    } catch (Exception e) {
+	        return false;
+	    }
 	}
 
 	public boolean isApplicationIDPreviewPagePageDisplayed() {
