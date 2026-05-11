96ecb7be66d7a2790e4ff87098ec48e603c99c18
GOKULRAJ136 <110164849+GOKULRAJ136@users.noreply.github.com>
2026-05-11 11:52:56 +0530

    Corrected Import
    Signed-off-by: GOKULRAJ136 <110164849+GOKULRAJ136@users.noreply.github.com>

--- android/app/src/main/java/io/mosip/registration_client/api_services/AuthenticationApi.java
+++ android/app/src/main/java/io/mosip/registration_client/api_services/AuthenticationApi.java
@@ -36,6 +36,7 @@ import io.mosip.registration.clientmanager.service.LoginService;
 import io.mosip.registration.clientmanager.spi.AuditManagerService;
 import io.mosip.registration.clientmanager.spi.SyncRestService;
 import io.mosip.registration.clientmanager.util.SyncRestUtil;
+import io.mosip.registration.clientmanager.R;
 import io.mosip.registration_client.UploadBackgroundService;
 import io.mosip.registration_client.model.AuthResponsePigeon;
 import retrofit2.Call;
@@ -72,7 +73,7 @@ public class AuthenticationApi implements AuthResponsePigeon.AuthResponseApi {
         this.globalParamRepository = globalParamRepository;
         sharedPreferences = this.context.
                 getSharedPreferences(
-                        this.context.getString(io.mosip.registration.clientmanager.R.string.app_name),
+                        this.context.getString(R.string.app_name),
                         Context.MODE_PRIVATE);
     }
 
