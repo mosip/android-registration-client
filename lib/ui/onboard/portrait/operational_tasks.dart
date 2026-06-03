15919b69d37f8b5ff5189d107453607dbf05f6fe
Sachin S P <52343650+SachinPremkumar@users.noreply.github.com>
2026-06-03 16:01:27 +0530

    US1-upadted the code rabbit review
    Signed-off-by: Sachin S P <52343650+SachinPremkumar@users.noreply.github.com>

--- lib/ui/onboard/portrait/operational_tasks.dart
+++ lib/ui/onboard/portrait/operational_tasks.dart
@@ -12,7 +12,6 @@ import 'package:percent_indicator/linear_percent_indicator.dart';
 import 'package:provider/provider.dart';
 import 'package:registration_client/provider/global_provider.dart';
 import 'package:registration_client/provider/approve_packets_provider.dart';
-import 'package:registration_client/provider/global_provider.dart';
 import 'package:registration_client/ui/onboard/portrait/task_card.dart';
 import 'package:registration_client/ui/onboard/widgets/home_page_card.dart';
 import 'package:registration_client/utils/app_config.dart';
