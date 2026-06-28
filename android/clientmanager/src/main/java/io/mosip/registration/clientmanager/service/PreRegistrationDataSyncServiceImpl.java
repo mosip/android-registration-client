3c1de929be1b2217aaa7c5d733c001cc60502988
Sachin S P <52343650+SachinPremkumar@users.noreply.github.com>
2026-06-28 18:36:55 +0530

    fixed coderabbit review
    Signed-off-by: Sachin S P <52343650+SachinPremkumar@users.noreply.github.com>

--- android/clientmanager/src/main/java/io/mosip/registration/clientmanager/service/PreRegistrationDataSyncServiceImpl.java
+++ android/clientmanager/src/main/java/io/mosip/registration/clientmanager/service/PreRegistrationDataSyncServiceImpl.java
@@ -222,13 +222,6 @@ public class PreRegistrationDataSyncServiceImpl implements PreRegistrationDataSy
             }
         } catch (Exception e) {
             Log.e(TAG,ERROR_FETCH_PRE_REG_PACKET, e);
-            try {
-                RegistrationDto registrationDto = this.registrationService.getRegistrationDto();
-                registrationDto.getDocuments().clear();
-                registrationDto.getDemographics().clear();
-            } catch (Exception inner) {
-                Log.e(TAG, "No active registration to roll back", inner);
-            }
         }
         return attributeData;
     }
@@ -244,11 +237,9 @@ public class PreRegistrationDataSyncServiceImpl implements PreRegistrationDataSy
             Log.i(TAG,"Pre-Registration ID is not present downloading {}"+ preRegistrationId);
             try {
                 preRegistration = downloadAndSavePacket(preRegistrationId, lastUpdatedTimeStamp);
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-                throw new RuntimeException(e);
-            } catch (ExecutionException e) {
-                throw new RuntimeException(e);
+            } catch (ExecutionException | InterruptedException e) {
+                this.registrationService.getRegistrationDto().getDocuments().clear();
+                this.registrationService.getRegistrationDto().getDemographics().clear();
             }
             return preRegistration;
         }
@@ -260,11 +251,9 @@ public class PreRegistrationDataSyncServiceImpl implements PreRegistrationDataSy
             Log.i(TAG,"Pre-Registration ID is not up-to-date downloading {}"+ preRegistrationId);
             try {
                 preRegistration = downloadAndSavePacket(preRegistrationId, lastUpdatedTimeStamp);
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-                throw new RuntimeException(e);
-            } catch (ExecutionException e) {
-                throw new RuntimeException(e);
+            } catch (ExecutionException | InterruptedException e) {
+                this.registrationService.getRegistrationDto().getDocuments().clear();
+                this.registrationService.getRegistrationDto().getDemographics().clear();
             }
         }
         return preRegistration;
