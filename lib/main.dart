d9e8bd7fa02b92f52446121ad3188e646069676e
nithesh <nitheshbachadka017@gmail.com>
2026-08-27 01:08:39 +0530

    feat(telemetry): track application cold startup duration for AC3
    Signed-off-by: nithesh <nitheshbachadka017@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,7 @@ import 'package:flutter_driver/driver_extension.dart';
 import 'package:flutter/services.dart';
 import 'package:registration_client/utils/inactivity_tracker.dart';
 import 'package:restart_app/restart_app.dart';
+import 'package:registration_client/utils/telemetry_service.dart';
 
 final GlobalKey<NavigatorState> rootNavigatorKey = GlobalKey<NavigatorState>();
 final GlobalKey<ScaffoldMessengerState> rootScaffoldMessengerKey =
@@ -32,17 +33,31 @@ GlobalKey<ScaffoldMessengerState>();
 const String _syncRestartChannel = 'io.mosip.registration_client/sync_restart';
 
 void main() async {
-  enableFlutterDriverExtension(enableTextEntryEmulation: false);
-  WidgetsFlutterBinding.ensureInitialized();
-  _setupSyncRestartChannel();
-  final GlobalProvider appLanguage = GlobalProvider();
-  await FlutterConfig.loadEnvVariables();
-  await appLanguage.fetchLocale();
-  runApp(
-    const RestartWidget(child: RegistrationClientApp()),
-  );
-}
+final double appInitTimestamp = DateTime.now().millisecondsSinceEpoch.toDouble();
 
+enableFlutterDriverExtension(enableTextEntryEmulation: false);
+WidgetsBinding.ensureInitialized();
+
+// Log startup metric once the first frame renders
+WidgetsBinding.instance.addPostFrameCallback((_) {
+final double renderCompleteTimestamp = DateTime.now().millisecondsSinceEpoch.toDouble();
+final double startupTimeMs = renderCompleteTimestamp - appInitTimestamp;
+
+TelemetryService.instance.logPerformanceMetric(
+  'registration.app.startup_time',
+  startupTimeMs,
+  'ms',
+);
+});
+
+_setupSyncRestartChannel();
+final GlobalProvider appLanguage = GlobalProvider();
+await FlutterConfig.loadEnvVariables();
+await appLanguage.fetchLocale();
+runApp(
+const RestartWidget(child: RegistrationClientApp()),
+);
+}
 /// Max retries when showing the sync-restart dialog if context/localizations
 /// are not ready yet (e.g. native call before widget tree is mounted).
 const int _syncRestartDialogMaxRetries = 10;
